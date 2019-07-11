ec722d410538be5e9a5b892fd1c5c54bc404e365
Jack Andersen <jacksfunny10@gmail.com>
2019-07-11 12:02:31 -0400

    fix

--- models/Bookchapt.js
+++ models/Bookchapt.js
@@ -3,7 +3,7 @@ const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 
 var bookSchema = Schema( {
-  bookchapt: Number,
+  number: Number,
 } );
 
 module.exports = mongoose.model( 'Book', bookSchema );

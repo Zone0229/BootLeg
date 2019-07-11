80de05150e2060a19dfabd490e902a3aebd8b2d0
dovushman <dushman0724@gmail.com>
2019-07-11 14:41:04 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
-        'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
+        'clientID': '160728964276-1v9ollpoopall2ji499bv78omfcjper9.apps.googleusercontent.com',
+        'clientSecret'  : 'hVOqEyZ6JtY51MAg1e6Lo0MM',
         'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
     }
 
